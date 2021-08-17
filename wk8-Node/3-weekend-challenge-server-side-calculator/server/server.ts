863a862790383adda19678dfc40327cb55166807
AJ Wells <wells19d@gmail.com>
2021-08-17 16:23:11 -0500

    setting up server / installing dependancies

--- wk8-Node/3-weekend-challenge-server-side-calculator/server/server.ts
+++ wk8-Node/3-weekend-challenge-server-side-calculator/server/server.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 app.get('/', (req, res) => {
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`server using port:${PORT}`);
+    console.log('Listening on port', PORT);
 });
